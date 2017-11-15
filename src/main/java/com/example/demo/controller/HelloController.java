ce6ba250281f2355369beab4a8607165a27c9e05
jinkens <171443519@qq.com>
2017-11-16 00:01:13 +0800

    提交代码

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import com.example.demo.commom.CommomResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public CommomResult<String> hello(){
+
+    return new CommomResult<String>("0","成功");
+    }
+}
